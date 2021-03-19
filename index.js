37ddd84716f6308ddc21f7a49ecee5dc9bf263b3
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2021-03-20 00:42:44 +0300

    feat: add proper array support

--- index.js
+++ index.js
@@ -1,71 +1,62 @@
 // trim all string prpoerties of an object
-function trimStringProperties (obj) {
+function trimStringProperties(obj) {
+  if (obj !== null && typeof obj === 'object') {
+    Object.keys(obj).forEach(function(prop) {
+      // if the property is an object trim it too
+      if (typeof obj[prop] === 'object') {
+        return trimStringProperties(obj[prop]);
+      }
 
-    if (obj !== null && typeof obj === 'object') {
-
-        for ( var prop in obj ) {
-
-            // if the property is an object trim it too
-            if ( typeof obj[prop] === 'object' ) {
-                return trimStringProperties(obj[prop]);
-            } 
-            
-            // if it's a string remove begin and end whitespaces
-            if ( typeof obj[prop] === 'string' ) {
-                obj[prop] = obj[prop].trim();
-            }
-
-        }
-
-    }
-
+      // if it's a string remove begin and end whitespaces
+      if (typeof obj[prop] === 'string') {
+        obj[prop] = obj[prop].trim();
+      }
+    })
+  }
 }
 
-
 // trimRequest middleware: trim all request object: body, params, query
-var all = function ( req, res, next ) {
+const all = () => (req, res, next) => {
+  if (req.body) {
+    trimStringProperties(req.body);
+  }
 
-    if ( req.body ) {
-        trimStringProperties(req.body);
-    }
+  if (req.params) {
+    trimStringProperties(req.params);
+  }
 
-    if ( req.params ) {
-        trimStringProperties(req.params);
-    }
+  if (req.query) {
+    trimStringProperties(req.query);
+  }
 
-    if ( req.query ) {
-        trimStringProperties(req.query);
-    }
-
-    next();
-
+  next();
 }
 
 // trimBody middleware: trim only the body object
-var body = function (req, res, next) {
-    if ( req.body ) {
-        trimStringProperties(req.body);
-    }
-    next();
+const body = () => (req, res, next) => {
+  if (req.body) {
+    trimStringProperties(req.body);
+  }
+  next();
 }
 
-var param = function (req, res, next) {
-    if ( req.params ) {
-        trimStringProperties(req.params);
-    }
-    next();
+const param = () => (req, res, next) => {
+  if (req.params) {
+    trimStringProperties(req.params);
+  }
+  next();
 }
 
-var query = function (req, res, next) {
-    if ( req.query ) {
-        trimStringProperties(req.query);
-    }
-    next();
+const query = () => (req, res, next) =>{
+  if (req.query) {
+    trimStringProperties(req.query);
+  }
+  next();
 }
 
 module.exports = {
-    all: all,
-    body: body,
-    param: param,
-    query: query
+  all: all,
+  body: body,
+  param: param,
+  query: query
 };
